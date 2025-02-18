4506b3eaa8ef17874448b7b4f8fd70c3e674e305
jaanekaraster <116946216+jaanekaraster@users.noreply.github.com>
2025-02-18 15:06:08 +0530

    updated files

--- client/src/components/ToggleLayerControl.jsx
+++ client/src/components/ToggleLayerControl.jsx
@@ -1,9 +1,8 @@
 const ToggleLayerControl = ({ label, isChecked, onToggle }) => (
-    <label style={{ display: "block", marginBottom: "10px" }}>
-      <input type="checkbox" checked={isChecked} onChange={onToggle} />
-      {label}
-    </label>
-  );
-  
-  export default ToggleLayerControl;
-  
+  <label style={{ display: "block", marginBottom: "10px" }}>
+    <input type="checkbox" checked={isChecked} onChange={onToggle} />
+    {label}
+  </label>
+);
+
+export default ToggleLayerControl;
